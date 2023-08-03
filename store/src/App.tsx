267d1a4d181992398f093e166904601ed475d643
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2023-08-03 16:49:30 +0200

    add logic for create list of products

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Card } from './Card/Card';
+import { PRODUCTS } from './constants/constants';
 
 function App() {
   return (
     <div>
-      <Card />
+      {PRODUCTS.map((item) => {
+        return <Card card={item} />
+      })}
     </div>
   );
 }
