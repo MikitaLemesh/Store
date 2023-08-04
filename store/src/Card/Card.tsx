d73485281a5c6c8339cef38b791d1813489de39f
MikitaLemesh <lemeshnikita999@gmail.com>
2023-08-04 14:13:31 +0200

    feat: add component CardButtons to component Card

--- store/src/Card/Card.tsx
+++ store/src/Card/Card.tsx
@@ -3,12 +3,16 @@ import styles from './card.module.css';
 import { ICard } from "../interfaces/card/interface";
 import { ImagesProduct } from "./ImageProduct/ImagesProduct";
 import { Information } from "./Information/Information";
+import { CardButtons } from "./CardButtons/CardButtons";
  
 export const Card = (props: ICard) => {
     return (
         <div>
-            <ImagesProduct imageFirst={props.card.imageFirst} name={props.card.brand} />
-            <Information stock={props.card.stock} type={props.card.type} rating={props.card.rating} price={props.card.price}/>
+            <div>
+                <ImagesProduct imageFirst={props.card.imageFirst} name={props.card.brand} />
+                <Information stock={props.card.stock} type={props.card.type} rating={props.card.rating} price={props.card.price}/>
+            </div>
+            <CardButtons />
         </div>
     )
 }
--- store/src/Card/CardButtons/CardButtons.tsx
+++ store/src/Card/CardButtons/CardButtons.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { ButtonDetails } from "./ButtonDetails/ButtonDetails";
+import { ButtonAdd } from "./ButtonAdd/ButtonAdd";
 
 export const CardButtons = () => {
     return (
-        <h1>Buttons</h1>
+        <div>
+            <ButtonAdd />
+            <ButtonDetails />
+        </div>
     )
 }
