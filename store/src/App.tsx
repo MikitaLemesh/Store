b99ae3fe377dceb4fdfd1589bd86d5e5a6df53ba
MikitaLemesh <lemeshnikita999@gmail.com>
2023-08-04 17:15:22 +0200

    feat: add in file App.tsx Routes

--- store/src/App.tsx
+++ store/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Card } from './Card/Card';
 import { PRODUCTS } from './constants/constants';
+import { CartPage } from './pages/CartPage/CartPage';
+import { DetailsPage } from './pages/DetailsPage/DetailsPage';
+import { Page } from './pages/Page';
+import { MainePage } from './pages/MainePage/MainePage';
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<Page />}>
+          <Route index element={<MainePage />}/>
+          <Route path='/cart' element={<CartPage />}/>
+          <Route path='/details' element={<DetailsPage />}/>
+          <Route path='*' />
+        </Route>
+      </Routes>
       {PRODUCTS.map((item) => {
         return <Card card={item} />
       })}
