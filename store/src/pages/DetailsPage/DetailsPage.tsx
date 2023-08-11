9f71a7ab7bddf619b71e01bdaafb278371df2346
MikitaLemesh <lemeshnikita999@gmail.com>
2023-08-11 19:34:02 +0200

    feat: add component ContainerDetailsPage to component DetailsPage

--- store/src/pages/DetailsPage/DetailsPage.tsx
+++ store/src/pages/DetailsPage/DetailsPage.tsx
@@ -5,15 +5,18 @@ import { TitleDetailsPage } from './TitleDetailsPage/TitleDetailsPage';
 import { ImagesContainer } from './ImagesContainer/ImagesContainer';
 import { InfoDetails } from './InfoDetails/InfoDetails';
 import { PriceContainer } from './PriceContainer/PriceContainer';
+import { Container } from './ContainerDetailsPage/ContainerDetailsPage';
 
 export const DetailsPage = () => {
   const item = PRODUCTS[idProduct.id];
   return (
     <DetailsContainer>
       <TitleDetailsPage brand={item.brand} />
-      <ImagesContainer imageFirst={item.imageFirst} imageSecond={item.imageSecond} imageThird={item.imageThird} brand={item.brand}/>
-      <InfoDetails description={item.description} price={item.price} rating={item.rating} type={item.type} stock={item.stock}/>
-      <PriceContainer price={item.price}/>
+      <Container>
+        <ImagesContainer imageFirst={item.imageFirst} imageSecond={item.imageSecond} imageThird={item.imageThird} brand={item.brand}/>
+        <InfoDetails description={item.description} price={item.price} rating={item.rating} type={item.type} stock={item.stock}/>
+        <PriceContainer price={item.price}/>
+      </Container>
     </DetailsContainer>
   )
 }
