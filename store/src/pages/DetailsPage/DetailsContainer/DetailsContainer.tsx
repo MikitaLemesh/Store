e194c40de24ae05ea83e797f0ec281572977b67a
MikitaLemesh <lemeshnikita999@gmail.com>
2023-08-11 19:37:01 +0200

    feat: add css stles for details page

--- store/src/pages/DetailsPage/DetailsContainer/DetailsContainer.tsx
+++ store/src/pages/DetailsPage/DetailsContainer/DetailsContainer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { IChildren } from '../../../interfaces/detailsPage/interface';
+import styles from './detailsContainer.module.css'
 
 export const DetailsContainer = (props: IChildren) => {
   return (
-    <div>
+    <div className={styles.details__container}>
       {props.children}
     </div>
   )
